71e0baf0dffbaa21dd64a803cf7d674d8f49eb07
john-ofagbe <john.ofagbe@gmail.com>
2026-06-30 17:23:46 -0700

    Add: top-k-elements-in-list - submission-59

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-59.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
+        Map<Integer, Integer> freqMap = new HashMap<>(); 
+        Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> freqMap.get(a) - freqMap.get(b));
+
+        for (int n: nums) {
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int n: freqMap.keySet()) {
+            minHeap.offer(n);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+
+
+
+    }
+}
