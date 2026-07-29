07180d3fd8d584329af88cc5b7f610ad065fce7b
john-ofagbe <john.ofagbe@gmail.com>
2026-07-29 08:07:22 -0700

    Add: permutation-string - submission-56

---
+++ Data Structures & Algorithms/permutation-string/submission-56.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] count = new int[26];
+        int[] windowCount = new int[26];
+
+        for (char c: s1.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int left = 0; 
+
+        for (int right = 0; right < s2.length(); right++) {
+            windowCount[s2.charAt(right) - 'a']++;
+            if (right - left + 1 > s1.length()) {
+                windowCount[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            if (Arrays.equals(count, windowCount)) return true;
+        }
+        return false;
+    }
+}
