81bdf2e03623a0740c2051e793b226244dd38b04
john-ofagbe <john.ofagbe@gmail.com>
2026-08-14 08:57:51 -0700

    Add: permutation-string - submission-57

---
+++ Data Structures & Algorithms/permutation-string/submission-57.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1Count = new int[26];
+        int[] window = new int[26];
+        for (char c: s1.toCharArray()) {
+            s1Count[c - 'a']++;
+        }
+        int left = 0; 
+
+        for (int right = 0; right < s2.length(); right++) {
+            window[s2.charAt(right) - 'a']++;
+            if (right - left + 1 > s1.length()) {
+                window[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            if (Arrays.equals(s1Count, window)) return true;
+        }
+        return false;
+    }
+}
