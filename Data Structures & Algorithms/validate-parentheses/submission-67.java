b04f22b91fa17484af62427f199d5a3f254c790b
john-ofagbe <john.ofagbe@gmail.com>
2026-09-11 08:25:23 -0700

    Add: validate-parentheses - submission-67

---
+++ Data Structures & Algorithms/validate-parentheses/submission-67.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false; // we have a closing bracket without an opening
+                char top = stack.pop();
+
+                if (
+                    (top == '(' && c != ')') ||
+                    (top == '{' && c != '}') ||
+                    (top == '[' && c != ']')
+                ) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
